08655ce92d8b9516403e881d0e3921f8c21b2641
BeaOwll <beatriz1115rosa@gmail.com>
2025-07-17 12:02:46 -0300

    Aulas iniciais de JavaScript

---
+++ Projetos/3_math/scripts.js
@@ -0,0 +1,15 @@
+//seleciona o maior numero
+let maior = Math.max(6,12,28,55,8);
+console.log(maior);
+
+//Seleciona o menor numero
+let menor = Math.min(6,12,28,55,8);
+console.log(menor);
+
+//Arredondar numeros
+let arredondar = Math.round(5.2934723);
+console.log(arredondar);
+
+//Arredondar numeros para cima
+let arredondarCima = Math.ceil(5.2934723);
+console.log(arredondarCima);
